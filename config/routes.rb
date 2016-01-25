2a66c799c10f7bfbb08b8bf8882be2ed8d0de2a9
purpleice9765 <purpleice9765@msn.com>
2016-01-25 12:10:26 +0800

    gem devise, add register, login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get "demo" => "demo#index"
+  root :to => "demo#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
