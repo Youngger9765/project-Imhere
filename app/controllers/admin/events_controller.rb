4adf5a99cfadd8c8bc7e668e8c27861a07b03107
purpleice9765 <purpleice9765@msn.com>
2016-01-27 10:34:13 +0800

    admin create event

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,5 +1,36 @@
 class Admin::EventsController < ApplicationController
 
   def index
+    @events = Event.all
+
+    if params[:event_id]
+      @event = event.find( params[:event_id] )
+    else
+      @event = Event.new
+    end
   end
+
+  def show
+    @event = event.find(params[:id])
+  end
+
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      flash[:notice] = "Create Success!"
+      redirect_to admin_events_path
+    else
+      flash[:alert] = "Create fail!"
+      render admin_events_path
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :content, :start_time, :end_time)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126022838) do
+ActiveRecord::Schema.define(version: 20160127021248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text     "content"
+    t.integer  "shared_people"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
