21e26d2b0230d83b9ff27d259ef800412f2f4d97
purpleice9765 <purpleice9765@msn.com>
2016-02-01 16:17:03 +0800

    API: activity add merchant info

--- app/controllers/api_v1/activities_controller.rb
+++ app/controllers/api_v1/activities_controller.rb
@@ -7,7 +7,8 @@ class ApiV1::ActivitiesController < ApplicationController
   def show
     @event = Event.find(params[:event_id])
     @activity = @event.activities.find(params[:id])
-    @milestone = @activity.activity_milestones.order("people ASC")
+    @milestones = @activity.activity_milestones.order("people ASC")
+    @merchants = @activity.merchants
   end
 
 end
