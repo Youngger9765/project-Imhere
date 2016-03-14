18a997c102a36f4a3c21852aa0043cfcff9d1f46
purpleice9765 <purpleice9765@msn.com>
2016-03-14 21:41:44 +0800

    fix public/uploads

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :linked_files, fetch(:linked_files, []).push( 'config/database.yml',
                                                   'config/shopify_webhook_secret.yml' )
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
