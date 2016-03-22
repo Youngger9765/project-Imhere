52d70062d4ff2e6fd52a8bf8e2eff334be9fb436
purpleice9765 <purpleice9765@msn.com>
2016-03-22 09:15:50 +0800

    try whenever

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,8 +40,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 set :passenger_restart_with_touch, true
 
-set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-
 namespace :deploy do
 
   after :restart, :clear_cache do
