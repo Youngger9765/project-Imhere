6e9f965dcc230a35df71f7a178d1af830e2b11d1
purpleice9765 <purpleice9765@msn.com>
2016-05-08 23:27:47 +0800

    set evn front variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,18 @@ class UsersController < ApplicationController
     token = params[:confirmation_token]
     user = User.find_by(:confirmation_token => token)
 
+    front_domain = YAML.load(File.read("#{Rails.root}/config/domain_variables.yml"))[Rails.env]
+    front_domain_name = front_domain["FRONT_DOMAIN"]
+
     if user
       user.confirmed_at = Time.now
       user.save
-      redirect_to "http://iamhere.smartboss.com.tw/star/index.php?eventsId=1#registerOk"
+
+      redirect_url = front_domain_name + "/star/index.php?eventsId=1#registerOk"
+      redirect_to redirect_url
     else
-      redirect_to "http://iamhere.smartboss.com.tw/star/index.php?eventsId=1#registerFail"
+      redirect_url = front_domain_name + "/star/index.php?eventsId=1#registerFail"
+      redirect_to redirect_url
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def front_domain_name
+    front_domain = YAML.load(File.read("#{Rails.root}/config/domain_variables.yml"))[Rails.env]
+    front_domain_name = front_domain["FRONT_DOMAIN"]
+  end
 end
