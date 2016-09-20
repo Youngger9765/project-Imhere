3e0c2c8a60531e09962a2975ab5dae6d51e4c595
youngger9765 <purpleice9765@msn.com>
2016-09-20 21:29:41 +0800

    modify user model for auth image

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,7 +60,23 @@ class User < ActiveRecord::Base
     if user
       user.fb_name = auth[:info][:name]
       user.fb_email = auth[:info][:email]
-      user.fb_head_shot = auth[:info][:image]
+
+      if auth[:info][:image].include?"graph.facebook"
+        user.fb_head_shot = auth[:info][:image]
+      else
+        user.fb_head_shot = "https://graph.facebook.com/" + auth.uid + "/picture?type=large"
+      end
+
+      if user.head_shot_file_name.nil? && auth.info.image.present?
+        if auth[:info][:image].include?"graph.facebook"
+          auth_image = auth[:info][:image]
+        else
+          auth_image = "https://graph.facebook.com/" + auth.uid + "/picture?type=large"
+        end
+        image_url = user.process_uri(auth_image)
+        user.head_shot = URI.parse(image_url)
+      end
+
       user.fb_token = auth.credentials.token
       user.fb_raw_data = auth
       user.skip_confirmation!
@@ -73,14 +89,27 @@ class User < ActiveRecord::Base
     if existing_user
 
       if existing_user.head_shot_file_name.nil? && auth.info.image.present?
-        image_url = existing_user.process_uri(auth.info.image)
+
+        if auth[:info][:image].include?"graph.facebook"
+          auth_image = auth[:info][:image]
+        else
+          auth_image = "https://graph.facebook.com/" + auth.uid + "/picture?type=large"
+        end
+
+        image_url = existing_user.process_uri(auth_image)
         existing_user.head_shot = URI.parse(image_url)
       end
 
       existing_user.fb_name = auth[:info][:name]
       existing_user.fb_email = auth[:info][:email]
-      existing_user.fb_head_shot = auth[:info][:image]
       existing_user.fb_uid = auth.uid
+
+      if auth[:info][:image].include?"graph.facebook"
+        existing_user.fb_head_shot = auth[:info][:image]
+      else
+        existing_user.fb_head_shot = "https://graph.facebook.com/" + auth.uid + "/picture?type=large"
+      end
+
       existing_user.fb_token = auth.credentials.token
       existing_user.fb_raw_data = auth
       existing_user.skip_confirmation!
@@ -95,7 +124,12 @@ class User < ActiveRecord::Base
     user.password = Devise.friendly_token[0,20]
 
     if auth.info.image.present?
-      image_url = user.process_uri(auth.info.image)
+      if auth[:info][:image].include?"graph.facebook"
+        auth_image = auth[:info][:image]
+      else
+        auth_image = "https://graph.facebook.com/" + auth.uid + "/picture?type=large"
+      end
+      image_url = user.process_uri(auth_image)
       user.head_shot = URI.parse(image_url)
     end
 
@@ -105,8 +139,14 @@ class User < ActiveRecord::Base
     user.fb_raw_data = auth
     user.fb_name = auth[:info][:name]
     user.fb_email = auth[:info][:email]
-    user.fb_head_shot = auth[:info][:image]
-    user.skip_confirmation! 
+
+    if auth[:info][:image].include?"graph.facebook"
+      user.fb_head_shot = auth[:info][:image]
+    else
+      user.fb_head_shot = "https://graph.facebook.com/" + auth.uid + "/picture?type=large"
+    end
+
+    user.skip_confirmation!
     user.save!
     return user
   end
