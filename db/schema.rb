fc8eb55116d97bf49480c78ae61b0c03a6a6d7bd
youngger9765 <purpleice9765@msn.com>
2016-10-02 22:46:35 +0800

    modify mail with request subdomain

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913141602) do
+ActiveRecord::Schema.define(version: 20161002142933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -322,6 +322,7 @@ ActiveRecord::Schema.define(version: 20160913141602) do
     t.datetime "click_user_miss_gifts_at", default: '2016-09-11 16:19:15'
     t.string   "avatar_gender"
     t.integer  "favoritings_count",        default: 0
+    t.string   "mail_subdomain"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
