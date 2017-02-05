663912692251779a28d804195600524e40650b69
youngger9765 <purpleice9765@msn.com>
2017-02-06 00:41:33 +0800

    add server GCE

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,12 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '161.202.37.146', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+# server '161.202.37.146', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+
+# GCE
+server '104.199.183.250', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+
+
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
