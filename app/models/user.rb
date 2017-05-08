bf36182ef41b02ce8c202acccc5d6cf4bd50d2f4
youngger9765 <purpleice9765@msn.com>
2017-05-08 09:53:07 +0800

    no confirm in GCE

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, 
+         :recoverable, :rememberable, :trackable, :validatable, 
+         # :confirmable, 
          :omniauthable, :omniauth_providers => [:facebook]
 
   validates :name, presence: true
